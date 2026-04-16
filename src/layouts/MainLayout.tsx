a2a00f944ff7109665562fcbb3863fd2ee7a8fce
Abhay G Joshi <joshiag.19@gmail.com>
2026-04-16 18:02:06 +0530

    Text Colour chnaged

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,8 +4,17 @@ import { Box, Typography } from "@mui/joy";
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <Box>
-      <Box sx={{ px: 2, py: 1.5, color: "white", bgcolor: "primary.500" }}>
-        <Typography level="h4">Incentive Portal</Typography>
+      <Box
+        sx={{
+          px: 2,
+          py: 1.5,
+          color: "rgb(255, 255, 255)",
+          bgcolor: "primary.500",
+        }}
+      >
+        <Typography level="h4" sx={{ color: "white" }}>
+          Incentive Portal
+        </Typography>
       </Box>
       <Box sx={{ p: 2, mx: "auto" }}>{children}</Box>
     </Box>
